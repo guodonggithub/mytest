ebe0e3b2f0c31bfe4b30953e856cb2b2bb0924d2
guodong <guodong@itcast.cn>
2018-03-02 17:55:40 +0800

    更爱代码的提交

--- test/src/test/xing.java
+++ test/src/test/xing.java
@@ -2,31 +2,7 @@ package test;
 
 public class xing {
 	public static void main(String[] args) {
-		System.out.println("     *");
-		for (int i = 4; i > 0 ; i--) {
-			for (int ii = 0; ii < i; ii++) {
-				System.out.print(" ");
-				
-			}
-			System.out.print("*");
-			for (int iii = 1; iii < 2*(5-i); iii++) {
-				System.out.print(" ");
-				}
-			System.out.println("*");
-			}
-		for (int i = 0; i <4 ; i++) {
-			for (int ii = 0; ii < i; ii++) {
-				System.out.print(" ");
-				
-			}
-			System.out.print("*");
-			for (int iii = 1; iii < 2*(5-i); iii++) {
-				System.out.print(" ");
-			}
-			System.out.println("*");
-		}
-		
-		System.out.println("     *");
+		System.out.println("第一次提交的测试");
 	}
 }
 
