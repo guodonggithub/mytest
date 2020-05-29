74a7ff2eafcf32b8322974ae4880df200d0307d9
sun_company <422792306@qq.com>
2020-05-29 16:22:01 +0800

    这里是对本次提交或者推送的描述内容

---
+++ test/src/test/AAAAA.java
@@ -0,0 +1,12 @@
+package test;/**
+ * @aothor Administrator
+ * @date 2020/5/29 16:17
+ */
+
+/**
+ * @aothor Administrator
+ * @date 2020/5/29 16:17
+ */
+public class AAAAA {
+
+}
---
+++ test/src/test/DDDDD.java
@@ -0,0 +1,16 @@
+package test;/**
+ * @aothor Administrator
+ * @date 2020/5/29 16:21
+ */
+
+/**
+ * @aothor Administrator
+ * @date 2020/5/29 16:21
+ */
+public class DDDDD {
+
+
+    public static void main(String[] args) {
+        System.out.println("hsidgsidhsig4158");
+    }
+}
